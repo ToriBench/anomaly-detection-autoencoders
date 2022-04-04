280f200a9ab996c084844cd4461116f271e9ed86
Victoria Bench <victoriab@bbd.co.za>
2022-04-04 16:48:38 +0200

    Accuracy matrix

--- autoencoder.py
+++ autoencoder.py
@@ -1,4 +1,5 @@
-# %load_ext tensorboard
+
+import os
 import tensorflow as tf
 from tensorflow import keras
 import datetime
@@ -7,127 +8,165 @@ import numpy as np
 from keras import layers, losses
 from keras.datasets import mnist
 from keras.models import Model
-import os
 from models import *
-import pickle
 from utils import *
+import io
+import sklearn.metrics
+import itertools
+import gc
+from packaging import version
 
-os.system("rm -rf ./logs/") 
-(x_train, y_train), (x_test, y_test)  = keras.datasets.mnist.load_data()
+
+os.system("rm -rf ./logs/")
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
 x_train = x_train.astype('float32')/255
 x_test = x_test.astype('float32')/255
 
-train_filter_norm = np.where(y_train == 1)
-test_filter_norm = np.where(y_test == 1)
-test_filter_anom = np.where(y_test == 1)
+test_data = x_test
+test_labels = y_test
 
-norm_train_data, norm_train_labels = x_train[train_filter_norm], y_train[train_filter_norm]
-norm_test_data, norm_test_labels = x_test[test_filter_norm], y_test[test_filter_norm]
+test_classes, test_class_occurances = np.unique(test_labels, return_counts=True)
+test_class_names = [str(x) for x in test_classes]
 
-anom_test_data = x_test[test_filter_anom]
+number_of_classes = len(test_classes)
+cm = np.zeros((number_of_classes, number_of_classes))  # Confusion matrix
+accuracy_matrix = []
 
-encoder_layer_list = [
-      layers.Flatten(),
-      layers.Dense(128, activation="relu"),
-      layers.Dense(64, activation="relu"),
-      layers.Dense(32, activation="relu")
+def plot_accuracy(test_labels, anomaly_predictions, norm_class):
+  results = []
+  for i in range(10):
+    lookup = np.where(test_labels==i)
+    pred_label = anomaly_predictions[lookup]
+    if i == norm_class:
+      true_label = np.zeros(lookup[0].shape[0])
+    else:
+      true_label = np.ones(lookup[0].shape[0])
+    accuracy = sum(pred_label == true_label)/len(lookup[0])
+    results.append(round(accuracy,2))
+  return results
+
+
+for norm_class in test_classes:
+
+    train_filter_norm = np.where(y_train == norm_class)
+    test_filter_norm = np.where(y_test == norm_class)
+
+    norm_train_data, norm_train_labels = x_train[train_filter_norm], y_train[train_filter_norm]
+    norm_test_data, norm_test_labels = x_test[test_filter_norm], y_test[test_filter_norm]
+
+    print("number of ", norm_class, "'s in test data: ", len(norm_test_labels))
+
+
+    encoder_layer_list = [
+        layers.Flatten(),
+        layers.Dense(128, activation="relu"),
+        layers.Dense(64, activation="relu"),
+        layers.Dense(32, activation="relu")
     ]
 
-decoder_layer_list = [
-      layers.Dense(64, activation="relu"),
-      layers.Dense(128, activation="relu"),
-      layers.Dense(784, activation="sigmoid"),
-      layers.Reshape((28, 28))
+    decoder_layer_list = [
+        layers.Dense(64, activation="relu"),
+        layers.Dense(128, activation="relu"),
+        layers.Dense(784, activation="sigmoid"),
+        layers.Reshape((28, 28))
     ]
 
-autoencoder_dnn = Autoencoder(encoder_layer_list,decoder_layer_list)
+    # encoder_layer_list = [
+    #     layers.Conv2D(32,(3, 3), activation='relu', padding='same', input_shape=(28, 28, 1)),
+    #     layers.MaxPooling2D((2, 2), padding='same'),
+    #     layers.Conv2D(16, (3, 3), activation='relu', padding='same'),
+    #     layers.MaxPooling2D((2, 2), padding='same'),
+    #     layers.Conv2D(8, (3, 3), activation='relu', padding='same')
+    #   ]
 
-autoencoder_dnn.compile(optimizer='adam', loss='binary_crossentropy')
+    # decoder_layer_list = [
+    #       layers.UpSampling2D((2, 2)),
+    #       layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
+    #       layers.UpSampling2D((2, 2)),
+    #       layers.Conv2D(1, (3, 3), activation='relu',padding='same'),
+    #       layers.Reshape((28,28))
+    # ]
 
-log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+    autoencoder = Autoencoder(encoder_layer_list, decoder_layer_list)
 
-history = autoencoder_dnn.fit(norm_train_data, norm_train_data, 
-          epochs=25, 
-          validation_data=(norm_test_data, norm_test_data),
-          callbacks=[tensorboard_callback])
+    autoencoder.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
 
-encoder_layer_list = [
-      layers.Conv2D(32,(3, 3), activation='relu', padding='same', input_shape=(28, 28, 1)),
-      layers.MaxPooling2D((2, 2), padding='same'),
-      layers.Conv2D(16, (3, 3), activation='relu', padding='same'),
-      layers.MaxPooling2D((2, 2), padding='same'),
-      layers.Conv2D(8, (3, 3), activation='relu', padding='same')
-    ]
+    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=log_dir, histogram_freq=1)
 
-decoder_layer_list = [
-      layers.UpSampling2D((2, 2)),
-      layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
-      layers.UpSampling2D((2, 2)),
-      layers.Conv2D(1, (3, 3), activation='relu',padding='same'),
-      layers.Reshape((28,28))
-]
+    early_stopping_callback = tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss',
+        min_delta=0,
+        patience=0,
+        verbose=0,
+        mode='auto',
+        baseline=None,
+        restore_best_weights=False
+    )
 
-# autoencoder_cnn = Autoencoder(encoder_layer_list=encoder_layer_list, decoder_layer_list=decoder_layer_list)
+    history = autoencoder.fit(norm_train_data, norm_train_data,
+                              epochs=25,
+                              validation_data=(norm_test_data, norm_test_data),
+                              callbacks=[tensorboard_callback, early_stopping_callback])
 
-# autoencoder_cnn.compile(optimizer='adam', loss='binary_crossentropy')
-# autoencoder_cnn.summary()
+    # Determine threshold from inputting normal test data
+    reconstructions = autoencoder.predict(norm_train_data)
+    losses = tf.keras.losses.mean_squared_error(tf.reshape(norm_train_data, [norm_train_data.shape[0], 784]), tf.reshape(reconstructions, [reconstructions.shape[0], 784]))
+    
+    # losses = np.zeros(len(norm_train_data))
+    # for i in range(len(norm_train_data)):
+    #     losses[i] = loss_function(norm_train_data[i], reconstructions[i]).numpy()
+    multiplier = 0.5 if (norm_class == 2 or 8) else 1
+    threshold = np.mean(losses) + multiplier * np.std(losses)
 
-log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    reconstructions = autoencoder.predict(test_data)
 
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+    anomaly_indices = []
+    number_of_inputs = len(test_data)
+    number_of_classes = len(test_classes)
+    anomaly_predictions = np.zeros(number_of_inputs)
 
-earlyStop_callback = tf.keras.callbacks.EarlyStopping(
-    monitor='val_loss',
-    min_delta=0,
-    patience=0,
-    verbose=0,
-    mode='auto',
-    baseline=None,
-    restore_best_weights=False
-)
+    test_loss = 0
 
-# history = autoencoder_cnn.fit(norm_train_data, norm_train_data, 
-#           epochs=16, 
-#           validation_data=(norm_test_data, norm_test_data),
-#           callbacks=[tensorboard_callback, earlyStop_callback])
+    for i in range(number_of_inputs):
+      test_loss = tf.keras.losses.mean_squared_error(tf.reshape(test_data[i],[784]), tf.reshape(reconstructions[i],[784]))
+      if (test_labels[i] == 1 and norm_class == 2) or (test_labels[i] == 2 and norm_class == 2):
+        print(f"norm_class = {norm_class} test_label = {test_labels[i]} test_loss = {test_loss}")
 
-autoencoder = autoencoder_dnn
-reconstructions = autoencoder.predict(norm_test_data)
-bce = tf.keras.losses.BinaryCrossentropy()
-losses = np.zeros(len(norm_test_data))
-print(bce(norm_test_data, reconstructions).numpy())
-for i in range(len(norm_test_data)):
-  losses[i]=bce(norm_test_data[i], reconstructions[i]).numpy()
+      if np.any(tf.math.greater(test_loss,threshold).numpy()):
+          anomaly_predictions[i] = 1
+          anomaly_indices.append(i)
+          
+    # plt.hist(test_loss[None, :], bins=50)
 
-print(np.mean(losses))
+    # plt.xlabel("Test loss")
 
-threshold = np.mean(losses) + 2 * np.std(losses)
-print(threshold)
+    # plt.ylabel("No of examples")
 
-dataset = anom_test_data
-reconstructions = autoencoder.predict(dataset)
-bce = tf.keras.losses.BinaryCrossentropy()
-anomaly_indices = []
-test_loss = np.zeros(dataset.shape[0])
-for i in range(dataset.shape[0]-1):
-  test_loss=bce(dataset[i], reconstructions[i]).numpy()
-  if test_loss > threshold:
-    anomaly_indices.append(i)
-counter = 0
+    # plt.show()
+    anomaly_label = np.where(y_test == norm_class, 0 ,1)
+    accuracy_matrix.append(plot_accuracy(test_labels, anomaly_predictions, norm_class))
+    
+    accuracy = sum(anomaly_predictions == anomaly_label)/len(test_data)
+    print(f"Class: {norm_class} Accuracy: {accuracy}")
 
-print('Number of anomalies detected: ', len(anomaly_indices))
-print('Number of anomalous inputs: ', len(dataset))
-print('Accuracy: ',(len(anomaly_indices)/len(dataset)))
+    # filtered = filter(lambda x: x<= 49, anomaly_indices)
+    # print_images(test_data[0:50], reconstructions[0:50],list(filtered), show_anomalies=False)
+    
 
-os.makedirs('out', exist_ok=True)
-save_data(
-  {
-    'out/dataset.txt': dataset,
-    'out/reconstructions.txt': reconstructions,
-    'out/anomaly_indices. txt': anomaly_indices
-  }
-)
-print_images(dataset, reconstructions, anomaly_indices, show_anomalies=True)
+
+cm_figure = plot_confusion_matrix(np.array(accuracy_matrix), class_names=test_class_names,
+                      title='Accuracy of each model per class', 
+                      axis_names=['Inputs', 'Trained Models'])
+
+
+
+
+
+  
+    
+
 
--- utils.py
+++ utils.py
@@ -1,44 +1,112 @@
 import matplotlib.pyplot as plt
 import pickle
+import tensorflow as tf
+import itertools
+import numpy as np
+
+
 def print_images(dataset, reconstructions,  anomaly_indices, show_anomalies):
-  n = anomaly_indices if show_anomalies else [x for x in range(len(dataset)) if x not in anomaly_indices]
-  
-  plt.figure(figsize=(200, 4))
+    n = anomaly_indices if show_anomalies else [
+        x for x in range(len(dataset)) if x not in anomaly_indices]
+    print(n)
 
-  counter = 0
-  for i in n:
-    # display original
-    plt.title("reconstructed")
-    ax = plt.subplot(2, len(n), counter + 1)
-    plt.imshow(dataset[i])
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+    plt.figure(figsize=(400, 4))
 
-    #display reconstruction
-    plt.title("reconstructed")
-    ax = plt.subplot(2, len(n), len(n)+ 1 + counter)
-    plt.imshow(reconstructions[i])
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-    counter +=1
-  plt.show()
+    counter = 0
+    for i in n:
+        # display original
+        plt.title("orig.")
+        ax = plt.subplot(2, len(n), counter + 1)
+        plt.imshow(dataset[i])
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # display reconstruction
+        plt.title("recons.")
+        ax = plt.subplot(2, len(n), len(n) + 1 + counter)
+        plt.imshow(reconstructions[i])
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        counter += 1
+    plt.show()
+
 
 def save_data(dictionary):
-  for file, data in dictionary.items():
-    with open(file, 'wb') as fp:
-      pickle.dump(data, fp)
+    for file, data in dictionary.items():
+        with open(file, 'wb') as fp:
+            pickle.dump(data, fp)
+
 
 def load_data(filenames):
-  dictionary = dict()
+    dictionary = dict()
 
-  for filename in filenames:
-    with open(filename, 'rb') as fp:
-      dictionary[filename] = pickle.load(fp)
+    for filename in filenames:
+        with open(filename, 'rb') as fp:
+            dictionary[filename] = pickle.load(fp)
 
-  return dictionary
+    return dictionary
 
 
-# data = load_data(['out/reconstructions.txt','out/dataset.txt', 'out/anomaly_indices.txt'])
-# print_images(data['out/dataset.txt'], data['out/reconstructions.txt'], data['out/anomaly_indices.txt'], show_anomalies=True)
+# def create_output_matrices(matrix, trained_model_class_index, classes, dataset, dataset_labels, number_of_label_occurances, reconstructions, loss_function, threshold):
+#     """
+#     Returns a 2D matrix containing the fraction of anomalies picked up.
+
+#     Args:
+#       cm (array, shape = [n, n]): a confusion matrix of integer classes
+#       class_names (array, shape = [n]): String names of the integer classes
+#     """
+#     anomaly_indices = []
+#     number_of_inputs = len(dataset)
+#     number_of_classes = len(classes)
+#     anomaly_predictions = np.zeros(number_of_inputs)
+#     cm = np.zeros((number_of_classes, number_of_classes))  # Confusion matrix
+#     test_loss = 0
+
+#     for i in enumerate(dataset):
+#         test_loss = loss_function(dataset[i], reconstructions[i]).numpy()
+#         if test_loss > threshold:
+#             anomaly_predictions[i] = 1
+#             anomaly_indices.append(i)
+
+#     for count, value in enumerate(classes):
+#         input_indices = np.where(dataset_labels == value)
+#         prediction = sum(anomaly_predictions[input_indices])
+#         cm[trained_model_class_index][count] = round(
+#             prediction/number_of_label_occurances[count], 2)
+
+    return cm, anomaly_indices
+
+
+def plot_confusion_matrix(cm, class_names, title, axis_names):
+    """
+    Returns a matplotlib figure containing the plotted confusion matrix.
+
+    Args:
+      cm (array, shape = [n, n]): a confusion matrix of integer classes
+      class_names (array, shape = [n]): String names of the integer classes
+    """
+    figure = plt.figure(figsize=(8, 8))
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(class_names))
+    plt.xticks(tick_marks, class_names, rotation=45)
+    plt.yticks(tick_marks, class_names)
+
+    # Compute the labels from the normalized confusion matrix.
+    labels = cm
+
+    # Use white text if squares are dark; otherwise black.
+    threshold = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        color = "white" if cm[i, j] > threshold else "black"
+        plt.text(j, i, labels[i, j], horizontalalignment="center", color=color)
+
+    plt.tight_layout()
+    plt.ylabel(axis_names[1])
+    plt.xlabel(axis_names[0])
+    plt.show()
+
+    return figure
