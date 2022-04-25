6bbb39048c0af1b1ee978dccdc8c918021f9418e
Zaeem Asvat <zaeema@bbd.co.za>
2022-04-25 12:03:46 +0200

    updated pca_clustering.py

--- pca_clustering.py
+++ pca_clustering.py
@@ -15,6 +15,7 @@ import math
 import scipy.spatial.distance as dist
 import random
 from sklearn.decomposition import PCA
+from tensorboard.plugins import projector
 
 def reduce_features(img, n_components=2):
     pca = PCA(n_components=n_components)
@@ -37,15 +38,38 @@ test_class_names = [str(x) for x in test_classes]
 
 for norm_class in test_classes:
 
-    print(f'PCA + K-Means Clustering for Normal Class: {norm_class}')
+    print(f'PCA for Normal Class: {norm_class}')
 
     # run PCA over all data
-    train_reduced = reduce_features(x_train)
-    test_reduced = reduce_features(x_test)
+    train_embeddings = reduce_features([x.flatten() for x in x_train])
+    test_embeddings = reduce_features([x.flatten() for x in x_test])
 
     # get normal and abnormal data splits
-    norm_train_data, norm_train_labels = x_train[np.where(y_train == norm_class)], y_train[np.where(y_train == norm_class)]
-    norm_test_data, norm_test_labels = x_test[np.where(y_test == norm_class)], y_test[np.where(y_test == norm_class)]
+    norm_train_data, norm_train_labels = train_embeddings[np.where(y_train == norm_class)], y_train[np.where(y_train == norm_class)]
+    norm_test_data, norm_test_labels = test_embeddings[np.where(y_test == norm_class)], y_test[np.where(y_test == norm_class)]
+
+    # label anomalous (1) and normal (0) data
+    norm_train_labels = [0 if x == norm_class else 1 for x in norm_train_labels]
+    norm_test_labels = [0 if x == norm_class else 1 for x in norm_test_labels]
+
+    # Set up a logs directory, so Tensorboard knows where to look for files.
+    log_dir = os.path.join('./logs', f'projection_{norm_class}')
+    os.makedirs(log_dir, exist_ok=True)
+
+    # Save Labels separately on a line-by-line manner.
+    with open(os.path.join(log_dir, f'metadata_{norm_class}.tsv'), "w") as f:
+        for l in norm_test_labels:
+            f.write(f"{int(l)}\n")
+
+    # Create a checkpoint from embedding
+    embeddings = tf.Variable(test_embeddings)
+    checkpoint = tf.train.Checkpoint(embedding=embeddings)
+    checkpoint.save(os.path.join(log_dir, f"embedding_{norm_class}.ckpt"))
+
+    # Set up config.
+    config = projector.ProjectorConfig()
+    embedding = config.embeddings.add()
 
     # visualize data
-    
+    embedding.metadata_path = 'metadata.tsv'
+    projector.visualize_embeddings(log_dir, config)
